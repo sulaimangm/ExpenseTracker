d2d4348366494d463a0e292786ec4234afd1cb0d
sulaimangm <sulaimangm@gmail.com>
2023-11-27 17:56:59 -0500

    Removing Unwanted Category Methods and Views

--- ExpenseTracker/Controllers/CategoryController.cs
+++ ExpenseTracker/Controllers/CategoryController.cs
@@ -24,24 +24,6 @@ namespace ExpenseTracker.Controllers
             return View(await _context.Categories.ToListAsync());
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // GET: Category/Create
         public IActionResult AddOrEdit(int id = 0)
         {
@@ -70,25 +52,6 @@ namespace ExpenseTracker.Controllers
             return View(category);
         }
 
-
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -102,11 +65,6 @@ namespace ExpenseTracker.Controllers
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool CategoryExists(int id)
-        {
-            return _context.Categories.Any(e => e.CategoryId == id);
         }
     }
 }
